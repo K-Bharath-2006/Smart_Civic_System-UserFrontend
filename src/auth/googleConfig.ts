eb42b031fbacedd73291691cf60016506bd4d4f3
Bharath K <gl.bharath2006@gmail.com>
2026-03-01 22:24:41 +0530

    Delete src/auth directory

--- src/auth/googleConfig.ts
+++
@@ -1,8 +0,0 @@
-import {GoogleSignin} from '@react-native-google-signin/google-signin';
-
-export const configureGoogleSignIn = () => {
-  GoogleSignin.configure({
-    webClientId:
-      '613041722407-sr9gnjifaul3ksnl20g38glbfvspvn45.apps.googleusercontent.com',
-  });
-};
